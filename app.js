fd1ca8def86362f9d1e54bf00b04b21bb1df4af2
Jason <bjason@gmail.com>
2014-01-31 22:49:14 -0800

    Example repos.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var github = require('octonode');
 
 var client = github.client();
 
-client.get('/users/pksunkara', {}, function (err, status, body, headers) {
+client.get('/users/jasoncbautista/repos', {}, function (err, status, body, headers) {
       console.log(body); //json object
 });
 
